868236276627eb265cf3cca1fc83b5ca3762fd9a
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-03 02:21:29 -0700

    Sign in sessions properly implemented???

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,20 @@ module SessionsHelper
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
+    # user.new_remember_token!
+    # cookies.permanent[:remember_token] = remember_token
+    # user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
+  end
+
+  def signed_in?
+    if !cookies[:remember_token]
+      false
+      # remember_token = User.new_remember_token
+      # cookies.permanent[:remember_token] = remember_token
+    else
+      !current_user.nil?
+    end
   end
 
   def current_user=(user)
@@ -14,6 +27,10 @@ module SessionsHelper
   def current_user
     remember_token  = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+    # remember_token = User.encrypted_token(cookies[:remember_token])
+    # @current_user ||= User.find_by(remember_token: remember_token)
+    # remember_token  = User.encrypt(cookies[:remember_token])
+    # @current_user ||= User.find_by(remember_token: remember_token)
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,39 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
 
+  # def User.new_remember_token
+  #   SecureRandom.urlsafe_base64
+  # end
+
+  def new_remember_token!
+    create_remember_token
+    save!(validate: false)
+  end
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
 
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token)
+    
+    # cost = if ActiveModel::SecurePassword.min_cost
+    #          BCrypt::Engine::MIN_COST
+    #        else
+    #          BCrypt::Engine::DEFAULT_COST
+    #        end
+    # BCrypt::Password.create(token, cost: cost)
   end
+
+  # def User.encrypted_token(token)
+  #   Digest::SHA1.hexdigest(token)
+  # end
 
   private
 
     def create_remember_token
+      # self.remember_token = User.encrypted_token(SecureRandom.urlsafe_base64)
       self.remember_token = User.encrypt(User.new_remember_token)
     end
-    
+
 end
