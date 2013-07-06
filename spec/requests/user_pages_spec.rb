96e6d9fdb9d5be2c8186caa2be89874f2c236dd7
Dave Schaefer <daveschaefer3@gmail.com>
2013-07-05 23:06:42 -0700

    Added micropost view partials

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -55,15 +55,7 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-
-  describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
-    before { visit user_path(user) }
-
-    it { should have_content(user.name) }
-    it { should have_title(user.name) }
-  end
-
+  
   describe "signup" do
 
     before { visit signup_path }
@@ -150,6 +142,23 @@ describe "User pages" do
       end
       before { patch user_path(user), params }
       specify { expect(user.reload).not_to be_admin }
+    end
+  end
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+
+    describe "microposts" do
+      it { should have_content(m1.content) }
+      it { should have_content(m2.content) }
+      it { should have_content(user.microposts.count) }
     end
   end
 
